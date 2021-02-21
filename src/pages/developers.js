291a314105ea00569c5dc6d1d7e4f5279f6603d9
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-02-21 13:04:11 -0800

    how much space do we want on the sides???/

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -1,18 +1,37 @@
+/**@jsxImportSource @emotion/react */
 import Navigation from "../components/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
+import {css} from '@emotion/react';
 
 
-export default function Developers() {
+function Developers() {
+    const row = css`
+        text-align: center;
+    `;
+
     return (
         <>
         <Navigation/>
-        <Container>
-            <Row>
+        <Container fluid>
+            <Row css={row}>
                 <Col>
-                    <h1>Meet the Developers!</h1>
+                    <h1 css={row}>Meet the Developers!</h1>
+                </Col>
+            </Row>
+            <Row css={row}>
+                <Col>
+                <h2> Anita</h2>
+                </Col>
+                <Col>
+                <h2> Kristina</h2>
+                </Col>
+                <Col>
+                <h2> ThuyVy</h2>
                 </Col>
             </Row>
         </Container>
     </>
     );
   }
+
+  export default Developers;
