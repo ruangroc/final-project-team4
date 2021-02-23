8e0cea7b0cfa2afeaf014d00acd97131e556eb1e
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-02-22 16:52:05 -0800

    paths for developers

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -2,9 +2,32 @@
 import Navigation from "../components/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
 import {css} from '@emotion/react';
+import {
+    Route,
+    Switch,
+    Link,
+    NavLink,
+    Redirect,
+    useParams,
+    useRouteMatch
+  } from 'react-router-dom';
+
+function Developer() {
+    const {developer} = useParams();
+    const { url, path } = useRouteMatch();
+
+    return (
+      <div>
+        <h2> Developer's Name </h2> 
+      </div>
+    );
+  }
 
 
 function Developers() {
+    const match = useRouteMatch();
+    const { url, path } = match;
+
     const row = css`
         text-align: center;
     `;
@@ -13,22 +36,29 @@ function Developers() {
         <>
         <Navigation/>
         <Container>
-            <Row css={row}>
-                <Col>
-                    <h1 css={row}>Meet the Developers!</h1>
-                </Col>
-            </Row>
-            <Row css={row}>
-                <Col>
-                <h2> Anita</h2>
-                </Col>
-                <Col>
-                <h2> Kristina</h2>
-                </Col>
-                <Col>
-                <h2> ThuyVy</h2>
-                </Col>
-            </Row>
+            <Switch>
+                <Route path={`${path}/:developer`}>
+                    <Developer />
+                </Route>
+                <Route exact path={path}>
+                    <Row css={row}>
+                        <Col>
+                            <h1 css={row}>Meet the Developers!</h1>
+                        </Col>
+                    </Row>
+                    <Row css={row}>
+                        <Col>
+                            <h2> Anita</h2>
+                        </Col>
+                        <Col>
+                            <h2> Kristina</h2>
+                        </Col>
+                        <Col>
+                            <h2> ThuyVy</h2>
+                        </Col>
+                    </Row>
+                </Route>
+            </Switch>
         </Container>
     </>
     );
