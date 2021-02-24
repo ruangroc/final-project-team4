34c7b7184b31420920a0c4dd6101144b10746ed3
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-02-23 22:33:00 -0800

    should work

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -1,16 +1,43 @@
 /**@jsxImportSource @emotion/react */
+import { useState, useEffect } from 'react';
 import Navigation from "../components/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
 import {css} from '@emotion/react';
-import {Route,Switch,useParams,useRouteMatch} from 'react-router-dom';
+import {Route,Switch,useParams,useRouteMatch, Redirect} from 'react-router-dom';
+import { get } from '../utils/api';
+
 
 function Developer() {
     const {developer} = useParams();
     const { url, path } = useRouteMatch();
+    const [dev, setDev] = useState([]);
+    
+    if(developer == "thuyvy"){
+        fetchspotifyuser("tweetynguy");
+    }else if(developer == "kristina"){
+        fetchspotifyuser("kxlee14");
+    }else if(developer == "anita"){
+        fetchspotifyuser("anitasmith98");
+    }else{
+        return <Redirect to="/error" />
+    }
+
+    async function fetchspotifyuser(user){
+        try{
+            const url = `https://api.spotify.com/v1/users/${user}`;
+            const result = await get(url);
+            console.log(result);
+            setDev(result || []);
+        } catch (e){
+            if ( e instanceof DOMException){
+                console.log("HTTP Request Aborted")
+            }
+        }
+    }
 
     return (
       <div>
-        <h2> Developer's Name </h2> 
+        <h2> {dev.display_name} </h2>
       </div>
     );
   }
@@ -46,7 +73,7 @@ function Developers() {
                             <h2> Kristina</h2>
                         </Col>
                         <Col>
-                            <h2> ThuyVy</h2>
+                             <h2> ThuyVy</h2>
                         </Col>
                     </Row>
                 </Route>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,15 @@
 import Navigation from "../components/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
 
 
 export default function Home() {
+
+      const handleLogin = () => {
+        
+      };
+
     return (
      <>
          <Navigation/>
@@ -10,6 +17,14 @@ export default function Home() {
              <Row>
                  <Col>
                      <h1> Spotify Interactive </h1>
+                     
+                 </Col>
+             </Row>
+             <Row>
+                 <Col>
+                    <Button variant="info" type="submit" onClick={handleLogin}>
+                        Login to spotify
+                    </Button>
                  </Col>
              </Row>
          </Container>
