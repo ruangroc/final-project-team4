a7e16a7a26187a81104062de77835ca827af6974
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2021-02-24 12:00:48 -0800

    added api call to get user's playlists

--- src/pages/collabplaylist.js
+++ src/pages/collabplaylist.js
@@ -2,10 +2,15 @@
 import Navigation from "../components/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
 import {css} from '@emotion/react';
+import {useState, useEffect} from 'react';
 
 
 function CollabPlaylist() {
-
+    useEffect(() => {
+        fetch('https://api.spotify.com/v1/me/playlists?access_token=BQC_qSgf463Wxd7hVDcXRQNmKxPaNYmPcVLztn-MjJBKnP9j8g8XdOR19fgr7xJOEnTeggsi-WMnFqHhf-HTqSNVBTWHzsHhQxTOch1iReia539BgGEt9aSJXT8CWUjDs9gT1NjINgkK_uynyQ&token_type=Bearer&expires_in=3600')
+        .then(response => response.json())
+        .then(data => console.log("data received:", data));
+    }, []);
 
     return (
         <>
