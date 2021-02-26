c0f7a25dcc1816391f59789fb887ad8ad0175186
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2021-02-25 16:26:55 -0800

    made developers page look nicer

--- src/pages/collabplaylist.js
+++ src/pages/collabplaylist.js
@@ -6,12 +6,6 @@ import {useState, useEffect} from 'react';
 
 
 function CollabPlaylist() {
-    useEffect(() => {
-        fetch('https://api.spotify.com/v1/me/playlists?access_token=BQC_qSgf463Wxd7hVDcXRQNmKxPaNYmPcVLztn-MjJBKnP9j8g8XdOR19fgr7xJOEnTeggsi-WMnFqHhf-HTqSNVBTWHzsHhQxTOch1iReia539BgGEt9aSJXT8CWUjDs9gT1NjINgkK_uynyQ&token_type=Bearer&expires_in=3600')
-        .then(response => response.json())
-        .then(data => console.log("data received:", data));
-    }, []);
-
     return (
         <>
         <Navigation/>
--- src/pages/developers.js
+++ src/pages/developers.js
@@ -1,7 +1,7 @@
 /**@jsxImportSource @emotion/react */
 import { useState, useEffect } from 'react';
 import Navigation from "../components/navbar";
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Card} from 'react-bootstrap';
 import {css} from '@emotion/react';
 import {Route,Switch,useParams,useRouteMatch, Redirect, Link} from 'react-router-dom';
 import { get } from '../utils/api';
@@ -47,33 +47,61 @@ function Developers() {
     const match = useRouteMatch();
     const { url, path } = match;
 
-    const row = css`
-        text-align: center;
+    const styles = css`
+        .row {
+            text-align: center;
+            margin-top: 1%;
+            margin-bottom: 1%;
+        }
+
+        .image {
+            width: 350px;
+            height: 300px;
+        }
+
+        .card {
+            width: 350px;
+            height: 100%;
+            background-color: #3BE378;
+        }
+
+        a {
+            color: black;
+        }
     `;
 
     return (
         <>
         <Navigation/>
-        <Container>
+        <Container css={styles}>
             <Switch>
                 <Route path={`${path}/:developer`}>
                     <Developer />
                 </Route>
                 <Route exact path={path}>
-                    <Row css={row}>
+                    <Row className="row">
                         <Col>
-                            <h1 css={row}>Meet the Developers!</h1>
+                            <h1 className="row">Meet the Developers!</h1>
                         </Col>
                     </Row>
-                    <Row css={row}>
+                    <Row className="row">
                         <Col>
-                            <Link to="/developers/anita"> <h2> Anita</h2> </Link>
+                            <Card className="row">
+                                <Card.Img className="image" src="http://3.bp.blogspot.com/--7gtJQo5mHE/UGMKHZapqmI/AAAAAAAAWGU/5X26Pgj_St4/s1600/funny-cat-pictures-017-005.jpg" />
+                                <Card.Text><Link to="/developers/anita"> <h2> Anita</h2> </Link></Card.Text>
+                            </Card>  
                         </Col>
                         <Col>
-                            <Link to="/developers/kristina"> <h2>Kristina</h2> </Link>
+                            <Card className="row">
+                                <Card.Img className="image" src="https://i.imgur.com/xPy88y5.png" />
+                                <Card.Text><Link to="/developers/kristina"> <h2>Kristina</h2> </Link></Card.Text>
+                            </Card>
                         </Col>
                         <Col>
-                            <Link to="/developers/thuyvy"> <h2> ThuyVy</h2> </Link>
+                            <Card className="row">
+                                <Card.Img className="image" src="http://allaboutcat.org/wp-content/uploads/2017/09/cat-sticking-tongue-out-2.jpg" />
+                                <Card.Text><Link to="/developers/thuyvy"> <h2> ThuyVy</h2> </Link></Card.Text>
+                            </Card>
                         </Col>
                     </Row>
                 </Route>
