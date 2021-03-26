1cb1a1c5b646a43704c963317131d2314333c28d
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2021-03-25 18:21:19 -0700

    fixed a typo

--- src/components/login.js
+++ src/components/login.js
@@ -12,7 +12,7 @@ function Login() {
             // redirectUri='http://localhost:3000/redirect'
             redirectUri='https://spotify-interactive.vercel.app/redirect'
             // clientID='164e3321d4714ea2b1d88976aeecb258'
-            clientId={process.env.REACT_APP_API_KEY}
+            clientID={process.env.REACT_APP_API_KEY}
             scopes={scopes}
             title="Login into Spotify"
         />
