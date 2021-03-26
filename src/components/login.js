9f3317c9c371585c99654f648b1d1b566798b0dd
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2021-03-25 18:29:17 -0700

    removed client id from app

--- src/components/login.js
+++ src/components/login.js
@@ -9,9 +9,7 @@ function Login() {
     return (
     <div>
         <SpotifyAuth
-            // redirectUri='http://localhost:3000/redirect'
             redirectUri='https://spotify-interactive.vercel.app/redirect'
-            // clientID='164e3321d4714ea2b1d88976aeecb258'
             clientID={process.env.REACT_APP_API_KEY}
             scopes={scopes}
             title="Login into Spotify"
