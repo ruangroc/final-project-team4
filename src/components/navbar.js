fd0eaa78f37c548c080d0e7c58542832c3566ac0
ThuyVy Nguyen <thuyvynguy@gmail.com>
2023-01-11 12:15:08 -0800

    lol

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,32 @@ import { Nav, Navbar } from "react-bootstrap";
 
 export default function Navigation() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">Spotify Interactive</Navbar.Brand>
+    <Navbar
+      bg="dark"
+      variant="light"
+      expand="lg"
+      style={{
+        backgroundImage: "url('background.jpeg')",
+        fontWeight: "bold",
+        color: "white",
+        fontFamily: "Coiny",
+      }}
+    >
+      <Navbar.Brand
+        style={{
+          fontWeight: "bold",
+          color: "white",
+          fontSize: "x-large",
+        }}
+        href="/"
+      >
+        Feline Groovy
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto"></Nav>
         <Nav>
-          <Nav.Link href="/userstats">cat-ify yourself</Nav.Link>
+          <Nav.Link href="/userstats">cat-ify your spotify</Nav.Link>
           <Nav.Link href="/combineplaylists">conCAT playlists</Nav.Link>
           <Nav.Link href="/developers">about</Nav.Link>
         </Nav>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,42 +1,58 @@
 import Navigation from "../components/navbar";
 
-import {Container, Row, Col, Jumbotron, Button} from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron, Button } from "react-bootstrap";
 
-import { useSelector } from 'react-redux';
-import { getAuth } from '../redux/selectors';
-import Login from '../components/login';
+import { useSelector } from "react-redux";
+import { getAuth } from "../redux/selectors";
+import Login from "../components/login";
 
 export default function Home() {
+  const auth = useSelector(getAuth);
+  const loggedIn = auth.loggedIn;
+  console.log("logged in: ", loggedIn);
 
-    const auth = useSelector(getAuth);
-    const loggedIn = auth.loggedIn;
-    console.log("logged in: ", loggedIn);
+  return (
+    <div>
+      <Navigation />
+      <Container>
+        <br></br>
 
-    return (
-        <div>
-            <Navigation/>
-            <Container >
-                <br></br>
+        <Jumbotron
+          style={{
+            backgroundColor: "#FFFFD7",
+          }}
+        >
+          <h1
+            style={{
+              color: "#54BEDC",
+              fontWeight: "bold",
+              fontFamily: "Coiny",
+              fontSize: "xxx-large",
+            }}
+          >
+            {" "}
+            feline' groovy{" "}
+          </h1>
+          <p>
+            {" "}
+            Learn and interact with your Spotify music taste with statistics and
+            cat visualizations. Find out what music the developers are listening
+            to and combine your playlists with our playlist combiner feature.{" "}
+          </p>
+          <br></br>
+          <br></br>
+          <Button
+            variant="outline-success"
+            href="https://github.com/osu-cs499-w21/final-project-team4"
+          >
+            Github{" "}
+          </Button>
+        </Jumbotron>
 
-                    <Jumbotron>
-                        <h1 style={{color: "#1aa34a", fontWeight: "bold"}}> Spotify Interactive </h1>
-                        <p> Learn and interact with your Spotify music taste with statistics and cat visualizations. Find out what music the developers are listening to and combine your playlists with our playlist combiner feature. </p>
-                        <br></br>
-                        <br></br>
-                        <Button variant="outline-success" href='https://github.com/osu-cs499-w21/final-project-team4'>Github </Button>
-                    </Jumbotron>
-
-                <Row>
-                    <Col>
-                        {loggedIn ? (
-                            <div>
-                            </div>
-                        ) : (
-                                <Login />
-                            )}
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-  }
+        <Row>
+          <Col>{loggedIn ? <div></div> : <Login />}</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
