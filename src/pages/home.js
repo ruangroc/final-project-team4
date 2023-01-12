e08857f861f731d467af154b600b276c207dc2a5
ThuyVy Nguyen <thuyvynguy@gmail.com>
2023-01-12 11:27:40 -0800

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,27 +16,58 @@ export default function Home() {
       <Navigation />
       <Container>
         <br></br>
-
+        <br></br>
+        <br></br>
         <Jumbotron
           style={{
             backgroundColor: "white",
           }}
         >
-          <h1
-            style={{
-              color: "#84A59D",
-              fontWeight: "bold",
-              fontFamily: "Coiny",
-              fontSize: "10vw",
-            }}
-          >
-            unpaws the music
-          </h1>
+          <Row>
+            <Col xs={8}>
+              <h1
+                style={{
+                  color: "#84A59D",
+                  fontWeight: "bold",
+                  fontFamily: "Coiny",
+                  fontSize: "7.5vw",
+                }}
+              >
+                unpaws the music
+              </h1>
+            </Col>
+            <Col>
+              <img
+                src="/cat.png"
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Col>
+          </Row>
           <p class="lead">
             Find out what kind of cat your music tastes create, learn more about
             your music habits, and combine your playlists with our playlist
             combiner feature here!
           </p>
+          <a
+            href="https://github.com/thuyvyng/spotifyInteractive"
+            class="badge badge-warning"
+          >
+            Github
+          </a>{" "}
+          <a
+            href="https://developer.spotify.com/documentation/web-api/"
+            class="badge badge-warning"
+          >
+            SpotifyAPI
+          </a>{" "}
+          <a
+            href="https://open.spotify.com/playlist/4YBShIMxSyiNX4M9q6dcxc?si=3c41c386b6454fcc"
+            class="badge badge-warning"
+          >
+            OurSpotifyPlaylist
+          </a>
         </Jumbotron>
 
         <Row>
